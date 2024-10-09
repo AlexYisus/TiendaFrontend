413676e4a3f6403eafcd25fc1092677b355f7f52
AlexYisus <alextaraguay20@gmail.com>
2024-10-09 17:36:12 -0500

    cambios menores 2

--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -235,67 +235,61 @@ const Shop = ({
 
                   {/* MOBILE FILTERS */}
                   <form onSubmit={e => onSubmit(e)} className="mt-4 border-t border-gray-200">
-                    <h3 className="sr-only">Categories</h3>
-                    <ul role="list" className="font-medium text-gray-900 px-2 py-3">
-                      {
-                        categories &&
-                        categories !== null &&
-                        categories !== undefined &&
-                        categories.map(category => {
-                          if (category.sub_categories.length === 0) {
-                            return (
-                              <div key={category.id} className=' flex items-center h-5 my-5'>
-                                <input
-                                  name='category_id'
-                                  onChange={e => onChange(e)}
-                                  value={category.id.toString()}
-                                  type='radio'
-                                  className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
-                                />
-                                <label className="ml-3 min-w-0 flex-1 text-gray-500">
-                                  {category.name}
-                                </label>
-                              </div>
-                            )
-                          } else {
-                            let result = []
+                    <h3 className="sr-only">Categorías</h3>
+                    <ul role="list" className="text-sm font-medium text-gray-900 space-y-4 pb-3">
+                    {
+                      categories &&
+                      categories !== null &&
+                      categories !== undefined &&
+                      categories.map(category => {
+                        if (category.sub_categories.length === 0) {
+                          return (
+                            <div key={category.id} className=' flex items-center h-5 my-5'>
+                              <input
+                                name='category_id'
+                                type='radio'
+                                className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                              />
+                              <label className="ml-3 min-w-0 flex-1 text-gray-500">
+                                {category.name}
+                              </label>
+                            </div>
+                          )
+                        } else {
+                          let result = []
+                          result.push(
+                            <div key={category.id} className='flex items-center h-5'>
+                              <input
+                                name='category_id'
+                                type='radio'
+                                className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                              />
+                              <label className="ml-3 min-w-0 flex-1 text-gray-500">
+                                {category.name}
+                              </label>
+                            </div>
+                          )
+
+                          category.sub_categories.map(sub_category => {
                             result.push(
-                              <div key={category.id} className='flex items-center h-5'>
+                              <div key={sub_category.id} className='flex items-center h-5 ml-2 my-5'>
                                 <input
                                   name='category_id'
-                                  onChange={e => onChange(e)}
-                                  value={category.id.toString()}
                                   type='radio'
                                   className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
                                 />
                                 <label className="ml-3 min-w-0 flex-1 text-gray-500">
-                                  {category.name}
+                                  {sub_category.name}
                                 </label>
                               </div>
                             )
+                          })
 
-                            category.sub_categories.map(sub_category => {
-                              result.push(
-                                <div key={sub_category.id} className='flex items-center h-5 ml-2 my-5'>
-                                  <input
-                                    name='category_id'
-                                    onChange={e => onChange(e)}
-                                    value={sub_category.id.toString()}
-                                    type='radio'
-                                    className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
-                                  />
-                                  <label className="ml-3 min-w-0 flex-1 text-gray-500">
-                                    {sub_category.name}
-                                  </label>
-                                </div>
-                              )
-                            })
-
-                            return result
-                          }
-                        })
-                      }
-                    </ul>
+                          return result
+                        }
+                      })
+                    }
+                  </ul>
 
                     <Disclosure as="div" className="border-t border-gray-200 px-4 py-6">
                       {({ open }) => (
@@ -360,7 +354,7 @@ const Shop = ({
                         <>
                           <h3 className="-mx-2 -my-3 flow-root">
                             <Disclosure.Button className="px-2 py-3 bg-white w-full flex items-center justify-between text-gray-400 hover:text-gray-500">
-                              <span className="font-sofiapro-regular text-gray-900">Mas Filtros</span>
+                              <span className="font-sofiapro-regular text-gray-900">Más Filtros</span>
                               <span className="ml-6 flex items-center">
                                 {open ? (
                                   <MinusSmIcon className="h-5 w-5" aria-hidden="true" />
@@ -433,7 +427,7 @@ const Shop = ({
                   className="p-2 -m-2 ml-4 sm:ml-6 text-gray-400 hover:text-gray-500 lg:hidden"
                   onClick={() => setMobileFiltersOpen(true)}
                 >
-                  <span className="sr-only">Filters</span>
+                  <span className="sr-only">Filtros</span>
                   <FilterIcon className="w-5 h-5" aria-hidden="true" />
                 </button>
               </div>
@@ -441,67 +435,14 @@ const Shop = ({
 
             <section aria-labelledby="products-heading" className="pt-6 pb-24">
               <h2 id="products-heading" className="sr-only">
-                Products
+                Productos
               </h2>
 
               <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-8 gap-y-10">
                 {/* Filters */}
                 <form onSubmit={e => onSubmit(e)} className="hidden lg:block">
                   <h3 className="font-bold text-2xl">Categorías</h3>
-                  <ul role="list" className="text-sm font-medium text-gray-900 space-y-4 pb-3">
-                    {
-                      categories &&
-                      categories !== null &&
-                      categories !== undefined &&
-                      categories.map(category => {
-                        if (category.sub_categories.length === 0) {
-                          return (
-                            <div key={category.id} className=' flex items-center h-5 my-5'>
-                              <input
-                                name='category_id'
-                                type='radio'
-                                className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
-                              />
-                              <label className="ml-3 min-w-0 flex-1 text-gray-500">
-                                {category.name}
-                              </label>
-                            </div>
-                          )
-                        } else {
-                          let result = []
-                          result.push(
-                            <div key={category.id} className='flex items-center h-5'>
-                              <input
-                                name='category_id'
-                                type='radio'
-                                className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
-                              />
-                              <label className="ml-3 min-w-0 flex-1 text-gray-500">
-                                {category.name}
-                              </label>
-                            </div>
-                          )
-
-                          category.sub_categories.map(sub_category => {
-                            result.push(
-                              <div key={sub_category.id} className='flex items-center h-5 ml-2 my-5'>
-                                <input
-                                  name='category_id'
-                                  type='radio'
-                                  className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
-                                />
-                                <label className="ml-3 min-w-0 flex-1 text-gray-500">
-                                  {sub_category.name}
-                                </label>
-                              </div>
-                            )
-                          })
-
-                          return result
-                        }
-                      })
-                    }
-                  </ul>
+                  
 
                   <h3 className="font-bold text-2xl mb-6">Filtrar por</h3>
 
