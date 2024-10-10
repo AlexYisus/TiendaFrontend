6c0ecfce63fdc2a8e46b31e46d5923804ce4a2ac
AlexYisus <alextaraguay20@gmail.com>
2024-10-10 00:34:32 -0500

    cambios menores 6

--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -443,40 +443,59 @@ const Shop = ({
                 <form onSubmit={e => onSubmit(e)} className="hidden lg:block">
                   <h3 className="font-bold text-2xl">Categorías</h3>
                   <ul role="list" className="text-sm font-medium text-gray-900 space-y-4 pb-3">
-  {
-    categories && categories.length > 0 && categories.map(category => (
-      <React.Fragment key={category.id}>
-        <div className='flex items-center h-5 my-5'>
-          <input
-            name='category_id'
-            type='radio'
-            value={category.id}  
-            className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
-          />
-          <label className="ml-3 min-w-0 flex-1 text-gray-500">
-            {category.name}
-          </label>
-        </div>
+                    {
+                      categories &&
+                      categories !== null &&
+                      categories !== undefined &&
+                      categories.map(category => {
+                        if (category.sub_categories.length === 0) {
+                          return (
+                            <div key={category.id} className=' flex items-center h-5 my-5'>
+                              <input
+                                name='category_id'
+                                type='radio'
+                                className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                              />
+                              <label className="ml-3 min-w-0 flex-1 text-gray-500">
+                                {category.name}
+                              </label>
+                            </div>
+                          )
+                        } else {
+                          let result = []
+                          result.push(
+                            <div key={category.id} className='flex items-center h-5'>
+                              <input
+                                name='category_id'
+                                type='radio'
+                                className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                              />
+                              <label className="ml-3 min-w-0 flex-1 text-gray-500">
+                                {category.name}
+                              </label>
+                            </div>
+                          )
 
-        {category.sub_categories && category.sub_categories.length > 0 && 
-          category.sub_categories.map(sub_category => (
-            <div key={sub_category.id} className='flex items-center h-5 ml-2 my-5'>
-              <input
-                name='subcategory_id' 
-                type='radio'
-                value={sub_category.id} 
-                className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
-              />
-              <label className="ml-3 min-w-0 flex-1 text-gray-500">
-                {sub_category.name}
-              </label>
-            </div>
-          ))
-        }
-      </React.Fragment>
-    ))
-  }
-</ul>
+                          category.sub_categories.map(sub_category => {
+                            result.push(
+                              <div key={sub_category.id} className='flex items-center h-5 ml-2 my-5'>
+                                <input
+                                  name='category_id'
+                                  type='radio'
+                                  className='focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded-full'
+                                />
+                                <label className="ml-3 min-w-0 flex-1 text-gray-500">
+                                  {sub_category.name}
+                                </label>
+                              </div>
+                            )
+                          })
+
+                          return result
+                        }
+                      })
+                    }
+                  </ul>
 
                   <h3 className="font-bold text-2xl mb-6">Filtrar por</h3>
 
