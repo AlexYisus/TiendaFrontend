e89d431ad98ae8e85185de709e73883d38b5a3df
AlexYisus <alextaraguay20@gmail.com>
2024-10-11 09:53:41 -0500

    cambios menores 28

--- src/containers/auth/ResetPasswordConfirm.js
+++ src/containers/auth/ResetPasswordConfirm.js
@@ -48,9 +48,9 @@ const ResetPasswordConfirm = ({
           <img
             className="mx-auto h-12 w-auto"
             src=""
-            alt=""
+            alt="Workflow"
           />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Escribe tu nueva contraseña</h2>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Ingresa tu nueva contraseña</h2>
           
         </div>
 
@@ -66,49 +66,43 @@ const ResetPasswordConfirm = ({
                     name="new_password"
                     value={new_password}
                     onChange={e=>onChange(e)}
-                    type={showPassword ? "text" : "password"}
-                    placeholder="*************************"
+                    type="password"
+                    placeholder="Contraseña Nueva"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
-                  <button type="button" onClick={togglePasswordVisibility} className="ml-2 text-xl absolute right-3 top-1/2 -translate-y-1/2">
-                      {showPassword ? <VscEyeClosed /> : <VscEye />}
-                    </button>
                 </div>
               </div>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Repetir Contraseña
+                  Repite tu contraseña
                 </label>
                 <div className="mt-1">
                   <input
                     name="re_new_password"
                     value={re_new_password}
                     onChange={e=>onChange(e)}
-                    type={showPassword ? "text" : "password"}
-                    placeholder="*************************"
+                    type="password"
+                    placeholder="Contraseña Nueva"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
-                  <button type="button" onClick={togglePasswordVisibility} className="ml-2 text-xl absolute right-3 top-1/2 -translate-y-1/2">
-                      {showPassword ? <VscEyeClosed /> : <VscEye />}
-                    </button>
                 </div>
               </div>
 
               <div>
-              {loading ? (
-                    <button className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-not-allowed" disabled>
-                      <PiSpinner className="text-2xl animate-spin" />
-                    </button>
-                  ) : (
-                    <button
-                      type="submit"
-                      className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Cambiar contraseña
-                    </button>
-                  )}
+                {loading ? 
+                <button
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                
+              </button>:
+              <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Cambiar Contraseña
+            </button>}
               </div>
             </form>
           </div>
