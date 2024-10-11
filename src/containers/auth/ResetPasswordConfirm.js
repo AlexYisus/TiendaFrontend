abcab81f856cda69eace1c896db4912037246459
AlexYisus <alextaraguay20@gmail.com>
2024-10-11 05:03:17 -0500

    cambios menores 26

--- src/containers/auth/ResetPasswordConfirm.js
+++ src/containers/auth/ResetPasswordConfirm.js
@@ -66,11 +66,14 @@ const ResetPasswordConfirm = ({
                     name="new_password"
                     value={new_password}
                     onChange={e=>onChange(e)}
-                    type="password"
-                    placeholder="Password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="************************"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
+                  <button type="button" onClick={togglePasswordVisibility} className="ml-2 text-xl absolute right-3 top-1/2 -translate-y-1/2">
+                      {showPassword ? <VscEyeClosed /> : <VscEye />}
+                    </button>
                 </div>
               </div>
               <div>
@@ -82,26 +85,30 @@ const ResetPasswordConfirm = ({
                     name="re_new_password"
                     value={re_new_password}
                     onChange={e=>onChange(e)}
-                    type="password"
-                    placeholder="Repeat Password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="************************"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
+                  <button type="button" onClick={togglePasswordVisibility} className="ml-2 text-xl absolute right-3 top-1/2 -translate-y-1/2">
+                      {showPassword ? <VscEyeClosed /> : <VscEye />}
+                    </button>
                 </div>
               </div>
 
               <div>
-                {loading ? 
-                <button
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-              </button>:
-              <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Cambiar Contraseña
-            </button>}
+              {loading ? (
+                    <button className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-not-allowed" disabled>
+                      <PiSpinner className="text-2xl animate-spin" />
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Cambiar contraseña
+                    </button>
+                  )}
               </div>
             </form>
           </div>
