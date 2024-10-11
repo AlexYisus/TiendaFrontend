341617f56edc531ec223ae5015b6dcce34668beb
AlexYisus <alextaraguay20@gmail.com>
2024-10-11 00:57:33 -0500

    cambios menores 14

--- src/containers/auth/ResetPassword.js
+++ src/containers/auth/ResetPassword.js
@@ -2,33 +2,40 @@ import Layout from '../../hocs/Layout'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import {connect} from 'react-redux'
-import { Navigate } from 'react-router'
-import { reset_password } from '../../redux/actions/auth'
-const ResetPassword = ({
-  reset_password,
+import { reset_password_confirm } from '../../redux/actions/auth'
+import {Navigate, useParams} from 'react-router'
+const ResetPasswordConfirm = ({
+  reset_password_confirm,
   loading
 }) => {
+  const params = useParams()
+
+  const [requestSent, setRequestSent] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0,0)
   }, [])
 
-  const [requestSent, setRequestSent] = useState(false);
-
   const [formData, setFormData] = useState({
-    email: '',
+    new_password: '',
+    re_new_password: ''
   })
 
   const { 
-    email,
+    new_password,
+    re_new_password
   } = formData;
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const onSubmit = e =>{
     e.preventDefault();
-    reset_password(email);
-    setRequestSent(true)
+    const uid = params.uid
+    const token = params.token
+
+    reset_password_confirm(uid, token, new_password, re_new_password)
+    if (new_password === re_new_password)
+      setRequestSent(true);
   }
 
   if (requestSent && !loading)
@@ -40,10 +47,10 @@ const ResetPassword = ({
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
             className="mx-auto h-12 w-auto"
-            src="https://electroferreteria.s3.us-east-2.amazonaws.com/MARKETING+CCAJ2-14.png"
-            alt=""
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
           />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Recuperar tu contraseña</h2>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Set your new password</h2>
           
         </div>
 
@@ -52,15 +59,31 @@ const ResetPassword = ({
             <form onSubmit={e=>onSubmit(e)} className="space-y-6">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Correo Electrónico
+                  Password
                 </label>
                 <div className="mt-1">
                   <input
-                    name="email"
-                    value={email}
+                    name="new_password"
+                    value={new_password}
                     onChange={e=>onChange(e)}
-                    type="email"
-                    placeholder="Email"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                  Repeat Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    name="re_new_password"
+                    value={re_new_password}
+                    onChange={e=>onChange(e)}
+                    type="password"
+                    placeholder="Repeat Password"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
@@ -72,13 +95,13 @@ const ResetPassword = ({
                 <button
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-
+            
               </button>:
               <button
               type="submit"
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Recuperar Contraseña
+              Reset password
             </button>}
               </div>
             </form>
@@ -93,5 +116,5 @@ const mapStateToProps = state => ({
 })
 
 export default connect(mapStateToProps, {
-  reset_password
-}) (ResetPassword)
+  reset_password_confirm
+}) (ResetPasswordConfirm)
