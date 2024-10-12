9df25c7ef204cc302cea5168749a88e6aca1a162
AlexYisus <alextaraguay20@gmail.com>
2024-10-12 02:35:44 -0500

    cambios menores 34

--- src/containers/pages/productDetail.jsx
+++ src/containers/pages/productDetail.jsx
@@ -106,7 +106,7 @@ const ProductDetail = ({
                   <div className="mt-10 flex sm:flex-col1">
                     {loading ? <button
 
-                      className="max-w-xs flex-1 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-[#005eff] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full" disabled
+                      className="max-w-xs flex-1 bg-[#005eff] border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-[#005eff] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full" disabled
                     >
                       <PiSpinner className="text-2xl animate-spin" />
                     </button> : <button
