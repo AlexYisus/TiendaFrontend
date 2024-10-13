3c11b6e8da2c4473bdd74997dd87ba17de3a0989
AlexYisus <alextaraguay20@gmail.com>
2024-10-13 04:33:29 -0500

    carrusel 2

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -10,9 +10,7 @@ import { Carousel } from 'react-responsive-carousel'; // Importa el carrusel
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Importa los estilos del carrusel
 import { GiDrill, GiHammerNails, GiNails, GiScrew, GiScrewdriver } from "react-icons/gi";
 import Parallax from 'parallax-js';
-import img1 from '../../assets/img/logo-ef.png'
-import img2 from '../../assets/img/img2.jpg'
-import img3 from '../../assets/img/img3.jpg'
+
 function Example({
   isAuthenticated,
   user,
@@ -114,24 +112,9 @@ function Example({
 
           </div>
         </div>
-      </div>
-      <div className="w-full max-w-2xl mt-8">
-              <Carousel showThumbs={false} autoPlay infiniteLoop>
-                <div>
-                  <img src={img1} alt="Imagen 1" />
-                 
-                </div>
-                <div>
-                  <img src={img2} alt="Imagen 2" />
-                  
-                </div>
-                <div>
-                  <img src={img3} alt="Imagen 3" />
-                  
-                </div>
-              </Carousel>
-            </div>
+      </div> 
     </section>
+    
   );
 }
 
