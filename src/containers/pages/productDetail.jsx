a3ebdb1262271ff6a54f5524ef258d0324c83a35
AlexYisus <alextaraguay20@gmail.com>
2024-12-18 12:57:59 -0500

    Descripton catalogo 4

--- src/containers/pages/productDetail.jsx
+++ src/containers/pages/productDetail.jsx
@@ -88,7 +88,7 @@ const ProductDetail = ({
     <Layout>
       <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          
+          <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
             <ImageGallery photo={product && product.photo} />
 
             {/* Product info */}
@@ -136,7 +136,7 @@ const ProductDetail = ({
             </div>
           </div>
         </div>
-      
+      </div>
     </Layout>
   );
 };
